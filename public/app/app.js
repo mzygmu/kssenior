0c89ab21e8fb012c3231350abcf901fc9ebfa835
Rafal Kazmierowski <rkazmierowski@gmail.com>
2015-03-22 13:36:51 +0100

    added vendors

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,21 @@
 'use strict';
-angular.module('app', ['ngResource', 'ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute', 'mainControllers']);
 
-angular.module('app').config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-  $routeProvider
-    .when('/', { 
-      templateUrl: '/partials/main', 
-      controller: 'mainCtrl'
-    });
-});
+// angular.module('app').config(function($routeProvider, $locationProvider) {
+//   $locationProvider.html5Mode(true);
+//   $routeProvider
+//     .when('/', { 
+//       templateUrl: '/partials/main', 
+//       controller: 'mainCtrl'
+//     });
+// });
+
+angular.module('app').config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/main',
+        controller: 'mainCtrl'
+      });
+  }
+]);
--- public/app/main/mainCtrl.js
+++ public/app/main/mainCtrl.js
@@ -1,5 +1,13 @@
 'use strict';
+var mainControllers = angular.module('mainControllers', []);
 
-angular.module('app').controller('mainCtrl', function($scope) {
-  $scope.myVar = "Hello Angular";
-});
+mainControllers.controller('mainCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    // $http.get('phones/phones.json').success(function(data) {
+    //   $scope.phones = data;
+    // });
+
+    $scope.myVar = "Hello Angular";
+  }
+]);
+
