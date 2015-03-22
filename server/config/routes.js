af01d235192dde9ce19bae5afb74f6f82079b261
Rafal Kazmierowski <rkazmierowski@gmail.com>
2015-03-22 16:50:35 +0100

    main content

--- server/config/routes.js
+++ server/config/routes.js
@@ -12,12 +12,12 @@ var mongoMessage;
 
 mongoMessage = 'test';
 
-  // app.get('/partials/*', function(req, res) {
-  //   res.render('../../public/app/' = req.params[0]);
-  // });
-  app.get('/partials/:partialPath', function(req, res) {
-    res.render('partials/' = req.params.partialPath);
+  app.get('/partials/*', function(req, res) {
+    res.render('../../public/app/' = req.params[0]);
   });
+  // app.get('/partials/:partialPath', function(req, res) {
+  //   res.render('partials/' = req.params.partialPath);
+  // });
   app.get('*', function(req, res) {
     res.render('index', {
       // temp
