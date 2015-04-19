ea769c3ab4a8e299190fd4b5cdcdb3663b5383f4
Rafal Kazmierowski <rkazmierowski@gmail.com>
2015-04-19 15:56:25 +0200

    Users

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,7 +6,8 @@ var auth = require('./auth'),
 
 module.exports = function(app) {
 
-  app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
+  app.get('/api/users', //auth.requiresRole('admin'), 
+    users.getUsers);
   app.post('/api/users', users.createUser);
   app.put('/api/users', users.updateUser);
 
