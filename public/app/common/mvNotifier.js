76486e7d7eedea6d11e4bc693680e34ca0b44143
Rafal Kazmierowski <rkazmierowski@gmail.com>
2015-04-19 11:51:57 +0200

    css

--- public/app/common/mvNotifier.js
+++ public/app/common/mvNotifier.js
@@ -5,6 +5,18 @@ angular.module('app').factory('mvNotifier', function(mvToastr) {
     notify: function(msg) {
       mvToastr.success(msg);
       console.log(msg);
-    }
+    },
+    error: function(msg) {
+      mvToastr.error(msg);
+      console.log(msg);
+    },
+    info: function(msg) {
+      mvToastr.info(msg);
+      console.log(msg);
+    },
+    warning: function(msg) {
+      mvToastr.warning(msg);
+      console.log(msg);
+    },
   }
 })
