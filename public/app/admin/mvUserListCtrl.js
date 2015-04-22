c3e0c4054743d780bb630f0048fcc3137110550c
Rafal Kazmierowski <rkazmierowski@gmail,com>
2015-04-22 23:59:56 +0200

    buttons

--- public/app/admin/mvUserListCtrl.js
+++ public/app/admin/mvUserListCtrl.js
@@ -1,12 +1,25 @@
 angular.module('app').controller('mvUserListCtrl', function($scope, mvUser, mvManageUsers) {
   $scope.users = mvUser.query();
   $scope.setAdmin = function(user) {
-  	if (mvManageUsers.isAdmin(user)) {
-  		mvManageUsers.removeRights(user);
-  	} else {
-  		mvManageUsers.addRights(user);
-  	}
+    if (mvManageUsers.isAdmin(user)) {
+      mvManageUsers.removeRights(user).then(function(res) {
+        $scope.users = mvUser.query();
+      }, function(err){
+        console.log(err);
+      });
+    } else {
+      mvManageUsers.addRights(user).then(function(res) {
+          $scope.users = mvUser.query();
+      }, function(err){
+        console.log(err);
+      });
+    }
   }
-  $scope.removeUser = mvManageUsers.removeUser;
+  $scope.removeUser = function(user) {
+    mvManageUsers.removeUser(user).then(function(res) {
+        $scope.users = mvUser.query();
+    }, function(err){
+      console.log(err);
+    });
   $scope.isAdmin = mvManageUsers.isAdmin;
 });
