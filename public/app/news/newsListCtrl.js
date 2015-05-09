5d9ee22650ebde99d5c8e8a1e84174e8a73d7245
Rafal Kazmierowski <rkazmierowski@gmail,com>
2015-05-09 22:42:43 +0200

    zawody

--- public/app/news/newsListCtrl.js
+++ public/app/news/newsListCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('newsListCtrl', function($scope, $modal, $log, newsList, mvIdentity, publishService, mvNotifier) {
+angular.module('app').controller('newsListCtrl', function($scope, $modal, $log, newsList, mvIdentity, publishService, mvNotifier, ConfirmService) {
   $scope.news = newsList.query();
   $scope.identity = mvIdentity;
 
@@ -24,14 +24,23 @@ angular.module('app').controller('newsListCtrl', function($scope, $modal, $log, 
   }
 
   $scope.removePost = function(post) {
-    publishService.removeNews(post).then(function(res) {
-      var index = $scope.news.indexOf(post);
-      $scope.news.splice(index, 1);
-      mvNotifier.notify('Ogłoszenie zostało usunięte');
-    }, function(err){
-      mvNotifier.error(err);
-      console.log(err);
-    });
+    var doRemove = function() {
+      publishService.removeNews(post).then(function(res) {
+        var index = $scope.news.indexOf(post);
+        $scope.news.splice(index, 1);
+        mvNotifier.notify('Ogłoszenie zostało usunięte');
+      }, function(err){
+        mvNotifier.error(err);
+        console.log(err);
+      });
+    }
+
+    var ask = {
+      title: 'usunięcie ogłoszenia',      
+      question: 'usunąć ogłoszenie z listy aktualności',
+      text: post.title
+    }
+    ConfirmService.confirm(ask, doRemove); 
   }
 
 });
