ae368bbbd157d9b787408b3bebbca1be54c26177
Rafal Kazmierowski <rkazmierowski@gmail,com>
2015-05-09 15:25:25 +0200

    zawody

--- public/app/competitions/competitionsModalCtrl.js
+++ public/app/competitions/competitionsModalCtrl.js
@@ -2,67 +2,28 @@ angular.module('app').controller('competitionsModalCtrl', function($scope, $moda
 
   if (competitions) {
   	// TODO set all fields
-    $scope.sectionTitle = content.sectionTitle;
-    $scope.text1 = content.text[0];
-
-    if (content.text[1]) {
-      $scope.text2 = content.text[1];
-    }
-    if (content.text[2]) {
-      $scope.text3 = content.text[2];
-    }
-    if (content.text[3]) {
-      $scope.text4 = content.text[3];
-    }
-    if (content.text[4]) {
-      $scope.text5 = content.text[4];
-    }
-    if (content.text[5]) {
-      $scope.text6 = content.text[5];
-    }
-    if (content.text[6]) {
-      $scope.text7 = content.text[6];
-    }
-    if (content.text[7]) {
-      $scope.text8 = content.text[7];
-    }
-  }
-
-  var getArray = function() {
-    var array = [];
-    if ($scope.text1) {
-      array.push($scope.text1);
-    }
-    if ($scope.text2) {
-      array.push($scope.text2);
-    }
-    if ($scope.text3) {
-      array.push($scope.text3);
-    }
-    if ($scope.text4) {
-      array.push($scope.text4);
-    }
-    if ($scope.text5) {
-      array.push($scope.text5);
-    }
-    if ($scope.text6) {
-      array.push($scope.text6);
-    }
-    if ($scope.text7) {
-      array.push($scope.text7);
-    }
-    if ($scope.text8) {
-      array.push($scope.text8);
-    }
-    return array;
+    $scope.title = competitions.title;
+    
+    $scope.notes = competitions.notes;
+    $scope.description = competitions.description;
   }
 
   var add = function() {
     var content = {
-      pageId: pageId,
-      sectionTitle: $scope.sectionTitle,
-      text: getArray(),
-      position: 0
+      title: $scope.title,
+      competition: ['3x20'],
+      date: new Date(),
+      notes: $scope.notes,
+      description: $scope.description
+
+  // title: {type:String, required:'{PATH} is required!'},
+  // competition: {type:[String], required:'{PATH} is required!'},
+  // date: {type:Date, required:'{PATH} is required!'},
+  // category: [String],
+  // types: [String],
+  // notes: String,
+  // description: String
+
     };
 
     competitionsService.publish(content).then(function() {
@@ -77,8 +38,19 @@ angular.module('app').controller('competitionsModalCtrl', function($scope, $moda
     var clone = angular.copy(content);
 
     var data = {
-      sectionTitle: $scope.sectionTitle,
-      text: getArray()
+      title: $scope.title,
+      competition: ['3x10'],
+      date: new Date(),
+      notes: $scope.notes,
+      description: $scope.description
+
+  // title: {type:String, required:'{PATH} is required!'},
+  // competition: {type:[String], required:'{PATH} is required!'},
+  // date: {type:Date, required:'{PATH} is required!'},
+  // category: [String],
+  // types: [String],
+  // notes: String,
+  // description: String
     };
     angular.extend(clone, data);
 
