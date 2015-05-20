998dda231d354b4437454d0a7e7f2e85493e123b
Rafal Kazmierowski <rkazmierowski@gmail,com>
2015-05-20 20:40:51 +0200

    content change

--- public/app/pages/contentModalCtrl.js
+++ public/app/pages/contentModalCtrl.js
@@ -3,65 +3,14 @@ angular.module('app').controller('contentModalCtrl', function($scope, $modalInst
   $scope.modalTitle = modalTitle;
   if (content) {
     $scope.sectionTitle = content.sectionTitle;
-    $scope.text1 = content.text[0];
-
-    if (content.text[1]) {
-      $scope.text2 = content.text[1];
-    }
-    if (content.text[2]) {
-      $scope.text3 = content.text[2];
-    }
-    if (content.text[3]) {
-      $scope.text4 = content.text[3];
-    }
-    if (content.text[4]) {
-      $scope.text5 = content.text[4];
-    }
-    if (content.text[5]) {
-      $scope.text6 = content.text[5];
-    }
-    if (content.text[6]) {
-      $scope.text7 = content.text[6];
-    }
-    if (content.text[7]) {
-      $scope.text8 = content.text[7];
-    }
-  }
-
-  var getArray = function() {
-    var array = [];
-    if ($scope.text1) {
-      array.push($scope.text1);
-    }
-    if ($scope.text2) {
-      array.push($scope.text2);
-    }
-    if ($scope.text3) {
-      array.push($scope.text3);
-    }
-    if ($scope.text4) {
-      array.push($scope.text4);
-    }
-    if ($scope.text5) {
-      array.push($scope.text5);
-    }
-    if ($scope.text6) {
-      array.push($scope.text6);
-    }
-    if ($scope.text7) {
-      array.push($scope.text7);
-    }
-    if ($scope.text8) {
-      array.push($scope.text8);
-    }
-    return array;
+    $scope.text = content.text;
   }
 
   var add = function() {
     var content = {
       pageId: pageId,
       sectionTitle: $scope.sectionTitle,
-      text: getArray(),
+      text: $scope.text,
       position: 0
     };
 
@@ -72,7 +21,6 @@ angular.module('app').controller('contentModalCtrl', function($scope, $modalInst
       mvNotifier.error(reason);
       $modalInstance.close(content);
     });
-    
   }
   
   var update = function() {
@@ -80,7 +28,7 @@ angular.module('app').controller('contentModalCtrl', function($scope, $modalInst
 
     var data = {
       sectionTitle: $scope.sectionTitle,
-      text: getArray()
+      text: $scope.text
     };
     angular.extend(clone, data);
 
--- server/models/PageContent.js
+++ server/models/PageContent.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var pageContent = mongoose.Schema({
   pageId: {type:String, required:'{PATH} is required!'},
   sectionTitle: String,
-  text: {type:[String], required:'{PATH} is required!'},
+  text: {type:String, required:'{PATH} is required!'},
   position: Number
 });
 var PageContent = mongoose.model('PageContent', pageContent);
@@ -12,66 +12,31 @@ function createDefaultPageContent() {
   PageContent.find({}).exec(function(err, collection) {
     if(collection.length === 0) {
       PageContent.create({pageId: 'footer', sectionTitle: 'GODZINY OTWARCIA', 
-        text: [
-          'Pn, Sr, Pt: 10.00 - 18.00',
-          'Nd: 9.00 - 14.00',
-          'Wt, Cz, So: Nieczynne'], position: 0});
+        text: 'Pn, Sr, Pt: 10.00 - 18.00\nNd: 9.00 - 14.00\nWt, Cz, So: Nieczynne', position: 0});
       PageContent.create({pageId: 'footer', sectionTitle: 'ADRES', 
-        text: [
-          'Strzelnica',
-          'ul. Zawodników 1',
-          'Gdańsk 80-729',
-          'klub@kssenior-gdansk.com',
-          'Tel: 881-444-452'], position: 0});
+        text: 'Strzelnica\nul. Zawodników 1\nGdańsk 80-729\nklub@kssenior-gdansk.com\nTel: 881-444-452', position: 0});
       PageContent.create({pageId: 'charges', sectionTitle: 'Wpisowe', 
-        text: [
-          'Dla nowoprzyjętych członków opłata wynosi 200,- zł. (młodzież do lat 20-stu wpisowego nie wpłaca)'], position: 0});
+        text: 'Dla nowoprzyjętych członków opłata wynosi 200,- zł. (młodzież do lat 20-stu wpisowego nie wpłaca)', position: 0});
       PageContent.create({pageId: 'charges', sectionTitle: 'Roczna składka członkowska wynosi:', 
-        text: [
-          '- młodzież do lat 20-stu opłaca 150,- zł',
-          '- pozostali (powyżej lat 20-stu) opłacają 200,- zł.',
-          'Składki członkowskie można opłacać w 2-ch ratach: za pierwsze pół-rocze (do końca marca każdego roku) oraz za drugie półrocze (do końca września każdego roku).'], position: 0});
+        text: '- młodzież do lat 20-stu opłaca 150,- zł\n- pozostali (powyżej lat 20-stu) opłacają 200,- zł.\nSkładki członkowskie można opłacać w 2-ch ratach: za pierwsze pół-rocze (do końca marca każdego roku) oraz za drugie półrocze (do końca września każdego roku).', position: 0});
       PageContent.create({pageId: 'charges', sectionTitle: 'Opłaty', 
-        text: [
-          '- opłata za egzamin na Patent strzelecki wynosi 400,- zł (młodzież do lat 18-stu egzaminu na Patent nie składa)',
-          '- opłata za Licencję strzelecką PZSS wynosi 50,- zł + opłata pocztowa 2,50 zł',
-          '- opłata za zawody (1 osobostart) wynosi 30,- zł.'], position: 0});
+        text: '- opłata za egzamin na Patent strzelecki wynosi 400,- zł (młodzież do lat 18-stu egzaminu na Patent nie składa)\n- opłata za Licencję strzelecką PZSS wynosi 50,- zł + opłata pocztowa 2,50 zł\n- opłata za zawody (1 osobostart) wynosi 30,- zł.', position: 0});
       PageContent.create({pageId: 'about', sectionTitle: 'Zarząd Klubu.', 
-        text: [
-          'Prezes Klubu: Szylberg Henryk,',
-          'Sekretarz Klubu: Andrzejewski Wiesław,',
-          'Skarbnik Klubu: Lewandowski Wojciech,',
-          'Członkowie Zarządu: Meller Katarzyna, Jakubaszek Jan, Łukowicz Henryk.',
-          'Kierownik Klubu: Leszek Tylak.'], position: 0});
+        text: 'Prezes Klubu: Szylberg Henryk,\nSekretarz Klubu: Andrzejewski Wiesław,\nSkarbnik Klubu: Lewandowski Wojciech,\nCzłonkowie Zarządu: Meller Katarzyna, Jakubaszek Jan, Łukowicz Henryk.\nKierownik Klubu: Leszek Tylak.', position: 0});
       PageContent.create({pageId: 'about', sectionTitle: 'Komisja Rewizyjna.', 
-        text: [
-          'Przewodniczący: Frańczak Marek,',
-          'Sekretarz: Tobiszewski Jarosław,',
-          'Członek Komisji: Kwapisiewicz Agnieszka.'], position: 0});
+        text: 'Przewodniczący: Frańczak Marek,\nSekretarz: Tobiszewski Jarosław,\nCzłonek Komisji: Kwapisiewicz Agnieszka.', position: 0});
       PageContent.create({pageId: 'about', sectionTitle: 'Władze klubu', 
-        text: [
-          'W latach 1952-1960 Kierownikiem Sekcji ZW LOK w Gdańsku (tak się wówczas nazywał Klub) był P. A. Magnuszewicz.',
-          'W latach 1961 -1993 Klubem kierował p. Krzysztof Osiński.',
-          'Od kwietnia 1994 r. do 7 kwietnia 2012 r. Kierownikiem Klubu był p. Jan Kubiak.',
-          'Obecnym Kierownikiem Klubu od maja 2012 r. jest p. Leszek Tylak.',
-          'Wielokrotnym Prezesem Klubu był p. Zbigniew Olszewski (przez 3 kadencje – 1995-2000, 2000-2005 oraz 2005-2010 r.)'], position: 0});
+        text: 'W latach 1952-1960 Kierownikiem Sekcji ZW LOK w Gdańsku (tak się wówczas nazywał Klub) był P. A. Magnuszewicz.\nW latach 1961 -1993 Klubem kierował p. Krzysztof Osiński.\nOd kwietnia 1994 r. do 7 kwietnia 2012 r. Kierownikiem Klubu był p. Jan Kubiak.\nObecnym Kierownikiem Klubu od maja 2012 r. jest p. Leszek Tylak.\nWielokrotnym Prezesem Klubu był p. Zbigniew Olszewski (przez 3 kadencje – 1995-2000, 2000-2005 oraz 2005-2010 r.)', position: 0});
       PageContent.create({pageId: 'about', sectionTitle: 'Krórka historia Klubu', 
-        text: [
-          'W latach 1952 – 1995 istniała Sekcja strzelecka Z.W.LOK w Gdańsku.Nazwę ”SENIOR” LOK Gdańsk Klub przyjął na zebraniu Sprawozdawczo-wyborczym członków w grudniu 1995 r. Decyzją Urzędu Miejskiego w Gdańsku z dnia 28 marca 2001 r.dopuszczono do użytkowania strzelnicę sportową z odkrytą osią 50-cio metrową 9-cio stanowiskową do broni kulowej (kaliber do 9mm pistoletowy) oraz osią krytą 15-sto metrową 4-ro stanowiskową do broni pneumatycznej (kaliber 4,5mm) zlokalizowaną w Gdańsku przy ul.Kopernika 16. W dniu 9 czerwca 2002 r.Polski Związek Strzelectwa Sportowego w Warszawie przyznał Klubowi ”SENIOR” LOK Gdańsk Licencję Nr LK-30/02/R. W roku 2005 uruchomiono dodatkowo 2-gą oś krytą 10-cio metrową 6-cio stanowiskową do strzelań z broni pneumatycznej (karabin i pistolet). Klub posiada Sekcję młodzieżową (strzelectwo sportowe oraz sporty obronne i letni biathlon) jak również Sekcję Grupy Powszechnej (strzelectwo sportowe). Od roku 2005 Klub pomaga co rok Gminie Trąbki Wielkie w organizacji Mistrzostw Gminy w strzelaniu z broni kulowej i pneumatycznej. W historii naszego Klubu biathlonista Przemysław Tymczuk był w latach 1994-1996 członkiem Kadry Narodowej Letniego Biathlonu a Medias Grzegorz w latach 2007-2008 był członkiem Kadry Narodowej Juniorów w pistolecie. Co rok w miesiącu wrześniu Klub ogłasza nabór nowych członków w wieku od lat 10-ciu.'], position: 0});
+        text: 'W latach 1952 – 1995 istniała Sekcja strzelecka Z.W.LOK w Gdańsku.Nazwę ”SENIOR” LOK Gdańsk Klub przyjął na zebraniu Sprawozdawczo-wyborczym członków w grudniu 1995 r. Decyzją Urzędu Miejskiego w Gdańsku z dnia 28 marca 2001 r.dopuszczono do użytkowania strzelnicę sportową z odkrytą osią 50-cio metrową 9-cio stanowiskową do broni kulowej (kaliber do 9mm pistoletowy) oraz osią krytą 15-sto metrową 4-ro stanowiskową do broni pneumatycznej (kaliber 4,5mm) zlokalizowaną w Gdańsku przy ul.Kopernika 16. W dniu 9 czerwca 2002 r.Polski Związek Strzelectwa Sportowego w Warszawie przyznał Klubowi ”SENIOR” LOK Gdańsk Licencję Nr LK-30/02/R. W roku 2005 uruchomiono dodatkowo 2-gą oś krytą 10-cio metrową 6-cio stanowiskową do strzelań z broni pneumatycznej (karabin i pistolet). Klub posiada Sekcję młodzieżową (strzelectwo sportowe oraz sporty obronne i letni biathlon) jak również Sekcję Grupy Powszechnej (strzelectwo sportowe). Od roku 2005 Klub pomaga co rok Gminie Trąbki Wielkie w organizacji Mistrzostw Gminy w strzelaniu z broni kulowej i pneumatycznej. W historii naszego Klubu biathlonista Przemysław Tymczuk był w latach 1994-1996 członkiem Kadry Narodowej Letniego Biathlonu a Medias Grzegorz w latach 2007-2008 był członkiem Kadry Narodowej Juniorów w pistolecie. Co rok w miesiącu wrześniu Klub ogłasza nabór nowych członków w wieku od lat 10-ciu.', position: 0});
       PageContent.create({pageId: 'join', sectionTitle: 'Deklaracja', 
-        text: [
-          '- z wypełnioną Deklaracją zgłosić się na posiedzenie Zarządu Klubu,',
-          '- po przyjęciu do Klubu opłacić obowiązujące składki,',
-          '- opłacić Legitymację członkowską PZSS – 32,50 zł'], position: 0});
+        text: '- z wypełnioną Deklaracją zgłosić się na posiedzenie Zarządu Klubu,\n- po przyjęciu do Klubu opłacić obowiązujące składki,\n- opłacić Legitymację członkowską PZSS – 32,50 zł', position: 0});
       PageContent.create({pageId: 'join', sectionTitle: 'Egzamin', 
-        text: [
-          '- przystąpić do egzaminu na PATENT STRZELECKI PZSS (test pisemny oraz praktyczny – strzelenie z pistoletu lub karabinka sportowego) wniosek o w/w Patent po wypełnieniu podpisuje lekarz medycyny sportowej. Koszt egzaminu wynosi 400,- zł.'], position: 0});
+        text: '- przystąpić do egzaminu na PATENT STRZELECKI PZSS (test pisemny oraz praktyczny – strzelenie z pistoletu lub karabinka sportowego) wniosek o w/w Patent po wypełnieniu podpisuje lekarz medycyny sportowej. Koszt egzaminu wynosi 400,- zł.', position: 0});
       PageContent.create({pageId: 'join', sectionTitle: 'Licencja strzelecka PZSS', 
-        text: [
-          '- po otrzymaniu Patentu wysyłamy kolejny wniosek do PZSS-u o Licencję strzelecką PZSS (koszt 50zł +/- opłata pocztowa). Na Wniosku należy również otrzymać podpis lekarza medycyny sportowej o zdolności uprawiania strzelectwa sportowego.',
-          '- po otrzymaniu Licencji należy brać udział w treningach i zawodach strzeleckich.'], position: 0});
+        text: '- po otrzymaniu Patentu wysyłamy kolejny wniosek do PZSS-u o Licencję strzelecką PZSS (koszt 50zł +/- opłata pocztowa). Na Wniosku należy również otrzymać podpis lekarza medycyny sportowej o zdolności uprawiania strzelectwa sportowego.\n- po otrzymaniu Licencji należy brać udział w treningach i zawodach strzeleckich.', position: 0});
     }
-  })
+  });
 }
 
 exports.createDefaultPageContent = createDefaultPageContent;
