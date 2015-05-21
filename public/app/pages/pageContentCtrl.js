dc94dbe9141fc7c890224173daea656affdfa633
Rafal Kazmierowski <rkazmierowski@gmail,com>
2015-05-21 19:57:45 +0200

    move

--- public/app/pages/pageContentCtrl.js
+++ public/app/pages/pageContentCtrl.js
@@ -132,14 +132,14 @@ angular.module('app').controller('pageContentCtrl', function($scope, $modal, $lo
 
   $scope.move = function(section) {
     console.log(section);
-
+    section.position =+ 1;
     for (var i in $scope.content) {
       if ($scope.content[i]._id === section._id) {
         $scope.content[i].position =+ 1;
         console.log($scope.content[i].position);
       }
     }
-
+    $scope.$apply();
     //pageContentService.move(section);
   }
 
