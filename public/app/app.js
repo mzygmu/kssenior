b33276c3e8d0ecfac50ed7b50c8e9717608d9aea
Rafał Kaźmierowski <rkazmierowski@gmail.com>
2015-05-31 14:51:52 +0200

    refactoring

--- public/app/app.js
+++ public/app/app.js
@@ -24,7 +24,7 @@ angular.module('app').config(function($routeProvider, $locationProvider, $sceDel
 
   $locationProvider.html5Mode(true);
   $routeProvider
-      .when('/', { templateUrl: '/partials/main/main', controller: 'kssMainCtrl'})
+      .when('/', { templateUrl: '/partials/main/main' })
       .when('/admin/users', { templateUrl: '/partials/admin/user-list',
         controller: 'kssUserListCtrl', resolve: routeRoleChecks.admin
       })
